e7791d989f4d2e24c18d7fc7a0c2fe26e3b83692
acurro9 <asbarxas.aaroncurro@gmail.com>
2023-12-10 23:21:55 +0000

    Se añade el control de errores

--- js/index.js
+++ js/index.js
@@ -1,40 +1,45 @@
 //Cambiar clases dependiendo del width
 $(window).resize(function () {
-  //En caso de width menor o igual a 900px
-  if ($(window).width() <= 900) {
-    showMenuP();
-    hideMenuNav();
-    showMenuImg();
-    showBtnM();
-    hideBtnC();
+  try {
+    if ($(window).width() <= 900) {
+      showMenuP();
+      hideMenuNav();
+      showMenuImg();
+      showBtnM();
+      hideBtnC();
 
-    changeCSS(".footer", "justify-content", "center");
-    changeCSS(".footer", "height", "15vh");
-    changeCSS("#footer_menu", "display", "none");
-    changeCSS(".projects_project", "margin-top", "10vh");
-    changeCSS("#head_menu", "height", "10vh");
+      changeCSS(".footer", "justify-content", "center");
+      changeCSS(".footer", "height", "15vh");
+      changeCSS("#footer_menu", "display", "none");
+      changeCSS(".projects_project", "margin-top", "10vh");
+      changeCSS("#head_menu", "height", "10vh");
 
-    changeCSS("#head_menu", "background-color", "transparent");
-    changeCSS("#head_menu", "backdrop-filter", "none");
-    changeCSS("#head_menu", "box-shadow", "none");
-    changeCSS("#head_menu", "border", "none");
-  } else {
-    changeCSS(".footer", "justify-content", "space-between");
-    changeCSS(".footer", "height", "10%");
-    changeCSS("#footer_menu", "display", "inline");
-    changeCSS(".projects_project", "margin-top", "26vh");
+      changeCSS("#head_menu", "background-color", "transparent");
+      changeCSS("#head_menu", "backdrop-filter", "none");
+      changeCSS("#head_menu", "box-shadow", "none");
+      changeCSS("#head_menu", "border", "none");
+    } else {
+      changeCSS(".footer", "justify-content", "space-between");
+      changeCSS(".footer", "height", "10%");
+      changeCSS("#footer_menu", "display", "inline");
+      changeCSS(".projects_project", "margin-top", "26vh");
 
-    changeCSS("#head_menu", "background-color", "transparent");
-    changeCSS("#head_menu", "backdrop-filter", "none");
-    changeCSS("#head_menu", "box-shadow", "none");
-    changeCSS("#head_menu", "border", "none");
-    hideMenuP();
-    showMenuNav();
-    hideMenuImg();
-    hideBtnM();
-    checkWidth();
-    hideBtnC();
+      changeCSS("#head_menu", "background-color", "transparent");
+      changeCSS("#head_menu", "backdrop-filter", "none");
+      changeCSS("#head_menu", "box-shadow", "none");
+      changeCSS("#head_menu", "border", "none");
+      hideMenuP();
+      showMenuNav();
+      hideMenuImg();
+      hideBtnM();
+      checkWidth();
+      hideBtnC();
+    }
+  } catch (err) {
+    //Se imprime el error en caso de haberlo
+    console.log("Error: " + err);
   }
+  //En caso de width menor o igual a 900px
 });
 
 //Muestra u oculta el menú desplegable
@@ -43,116 +48,136 @@ let btnM = document.getElementById("btnM");
 let btnC = document.getElementById("btnC");
 //Botón para abrir menú
 btnM.addEventListener("click", () => {
-  hideMenuP();
-  showMenuNav();
-  hideBtnM();
-  showBtnC();
-  showMenuImgC();
-  changeCSS("#head_menu", "background-color", "rgba(0, 0, 0, 0.6)");
-  changeCSS("#head_menu", "backdrop-filter", "blur(5px)");
-  changeCSS("#head_menu", "box-shadow", "0 0 40px rgba(8, 7, 16, 0.6)");
-  changeCSS("#head_menu", "border", "2px solid rgba(255, 255, 255, 0.1)");
-  changeCSS("#head_menu", "height", "15vh");
-  if ($(window).width() <= 900) {
-    changeCSS("#head_menu", "height", "100%");
-    changeCSS("#menuNavv2", "margin-top", "10rem");
+  try {
+    hideMenuP();
+    showMenuNav();
+    hideBtnM();
+    showBtnC();
+    showMenuImgC();
+    changeCSS("#head_menu", "background-color", "rgba(0, 0, 0, 0.6)");
+    changeCSS("#head_menu", "backdrop-filter", "blur(5px)");
+    changeCSS("#head_menu", "box-shadow", "0 0 40px rgba(8, 7, 16, 0.6)");
+    changeCSS("#head_menu", "border", "2px solid rgba(255, 255, 255, 0.1)");
+    changeCSS("#head_menu", "height", "15vh");
+    if ($(window).width() <= 900) {
+      changeCSS("#head_menu", "height", "100%");
+      changeCSS("#menuNavv2", "margin-top", "10rem");
+    }
+  } catch (err) {
+    //Se imprime el error en caso de haberlo
+    console.log("Error: " + err);
   }
 });
 //Botón para cerrar el menú
 btnC.addEventListener("click", () => {
-  showMenuP();
-  hideMenuNav();
-  showBtnM();
-  hideBtnC();
-  hideMenuImgC();
+  try {
+    showMenuP();
+    hideMenuNav();
+    showBtnM();
+    hideBtnC();
+    hideMenuImgC();
 
-  changeCSS("#head_menu", "background-color", "transparent");
-  changeCSS("#head_menu", "backdrop-filter", "none");
-  changeCSS("#head_menu", "box-shadow", "none");
-  changeCSS("#head_menu", "border", "none");
+    changeCSS("#head_menu", "background-color", "transparent");
+    changeCSS("#head_menu", "backdrop-filter", "none");
+    changeCSS("#head_menu", "box-shadow", "none");
+    changeCSS("#head_menu", "border", "none");
 
-  //En caso de width menor o igual a 900pxs
-  if ($(window).width() <= 900) {
-    changeCSS("#head_menu", "height", "10vh");
-    changeCSS("#menuNavv2", "margin-top", "0");
+    //En caso de width menor o igual a 900pxs
+    if ($(window).width() <= 900) {
+      changeCSS("#head_menu", "height", "10vh");
+      changeCSS("#menuNavv2", "margin-top", "0");
+    }
+  } catch (err) {
+    //Se imprime el error en caso de haberlo
+    console.log("Error: " + err);
   }
 });
 
 //Cambiar el menú al hacer scroll
 window.addEventListener("scroll", () => {
+  try {
+    if ($(window).width() > 900) {
+      //En caso de no estar en el top de la página
+      if ($(window).scrollTop() === 0) {
+        hideMenuP();
+        hideMenuImg();
+        hideBtnM();
+        showMenuP2();
+        showMenuNav();
+        hideMenuImgC();
+        hideBtnC();
+        changeCSS("#head_menu", "height", "23vh");
+        changeCSS("#head_menu", "background-color", "transparent");
+        changeCSS("#head_menu", "backdrop-filter", "none");
+        changeCSS("#head_menu", "box-shadow", "none");
+        changeCSS("#head_menu", "border", "none");
+      } else {
+        showMenuP();
+        showMenuImg();
+        showBtnM();
+        hideMenuP2();
+        hideMenuNav();
+        hideMenuImgC();
+        hideBtnC();
+        changeCSS("#head_menu", "height", "10vh");
+        changeCSS("#head_menu", "background-color", "transparent");
+        changeCSS("#head_menu", "backdrop-filter", "none");
+        changeCSS("#head_menu", "box-shadow", "none");
+        changeCSS("#head_menu", "border", "none");
+      }
+    }
+  } catch (err) {
+    //Se imprime el error en caso de haberlo
+    console.log("Error: " + err);
+  }
   //En caso de width mayor a 900px
-  if ($(window).width() > 900) {
-    //En caso de no estar en el top de la página
-    if ($(window).scrollTop() === 0) {
-      hideMenuP();
-      hideMenuImg();
-      hideBtnM();
-      showMenuP2();
-      showMenuNav();
-      hideMenuImgC();
-      hideBtnC();
-      changeCSS("#head_menu", "height", "23vh");
+});
+//Chequea el width al cargar la página para cambiar el menú
+function checkWidth() {
+  try {
+    //En caso de width menor o igual a 900px
+    if ($(window).width() <= 900) {
+      hideMenuNav();
+      showMenuImg();
+      showMenuP();
+      showBtnM();
+
+      changeCSS("#head_menu", "height", "10vh");
+      changeCSS(".projects_project", "margin-top", "2vh");
+      changeCSS(".projects_project", "background-color", "transparent");
+      changeCSS(".projects_project", "backdrop-filter", "none");
+      changeCSS(".projects_project", "box-shadow", "none");
+      changeCSS(".projects_project", "border", "none");
+      changeCSS("#footer_menu", "display", "none");
+      changeCSS(".footer", "justify-content", "center");
+      changeCSS(".footer", "height", "15vh");
       changeCSS("#head_menu", "background-color", "transparent");
       changeCSS("#head_menu", "backdrop-filter", "none");
       changeCSS("#head_menu", "box-shadow", "none");
       changeCSS("#head_menu", "border", "none");
     } else {
-      showMenuP();
-      showMenuImg();
-      showBtnM();
-      hideMenuP2();
-      hideMenuNav();
-      hideMenuImgC();
-      hideBtnC();
-      changeCSS("#head_menu", "height", "10vh");
-      changeCSS("#head_menu", "background-color", "transparent");
-      changeCSS("#head_menu", "backdrop-filter", "none");
-      changeCSS("#head_menu", "box-shadow", "none");
-      changeCSS("#head_menu", "border", "none");
-    }
-  }
-});
-//Chequea el width al cargar la página para cambiar el menú
-function checkWidth() {
-  //En caso de width menor o igual a 900px
-  if ($(window).width() <= 900) {
-    hideMenuNav();
-    showMenuImg();
-    showMenuP();
-    showBtnM();
+      //En caso de no estar en el top de la página
+      if ($(window).scrollTop() !== 0) {
+        hideMenuNav();
+        showMenuImg();
+        showBtnM();
+        showMenuP();
+        hideMenuP2();
 
-    changeCSS("#head_menu", "height", "10vh");
-    changeCSS(".projects_project", "margin-top", "2vh");
-    changeCSS(".projects_project", "background-color", "transparent");
-    changeCSS(".projects_project", "backdrop-filter", "none");
-    changeCSS(".projects_project", "box-shadow", "none");
-    changeCSS(".projects_project", "border", "none");
-    changeCSS("#footer_menu", "display", "none");
-    changeCSS(".footer", "justify-content", "center");
-    changeCSS(".footer", "height", "15vh");
-    changeCSS("#head_menu", "background-color", "transparent");
-    changeCSS("#head_menu", "backdrop-filter", "none");
-    changeCSS("#head_menu", "box-shadow", "none");
-    changeCSS("#head_menu", "border", "none");
-  } else {
-    //En caso de no estar en el top de la página
-    if ($(window).scrollTop() !== 0) {
-      hideMenuNav();
-      showMenuImg();
-      showBtnM();
-      showMenuP();
-      hideMenuP2();
-
-      changeCSS("#head_menu", "height", "10vh");
-      changeCSS(".projects_project", "margin-top", "26vh");
-      changeCSS("#footer_menu", "display", "visible");
-      changeCSS(".footer", "height", "10%");
-      changeCSS(".footer", "justify-content", "space-between");
-      changeCSS("#head_menu", "background-color", "transparent");
-      changeCSS("#head_menu", "backdrop-filter", "none");
-      changeCSS("#head_menu", "box-shadow", "none");
-      changeCSS("#head_menu", "border", "none");
+        changeCSS("#head_menu", "height", "10vh");
+        changeCSS(".projects_project", "margin-top", "26vh");
+        changeCSS("#footer_menu", "display", "visible");
+        changeCSS(".footer", "height", "10%");
+        changeCSS(".footer", "justify-content", "space-between");
+        changeCSS("#head_menu", "background-color", "transparent");
+        changeCSS("#head_menu", "backdrop-filter", "none");
+        changeCSS("#head_menu", "box-shadow", "none");
+        changeCSS("#head_menu", "border", "none");
+      }
     }
+  } catch (err) {
+    //Se imprime el error en caso de haberlo
+    console.log("Error: " + err);
   }
 }
 //Widget google maps
